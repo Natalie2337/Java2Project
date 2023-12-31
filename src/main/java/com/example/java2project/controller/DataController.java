2ee3798a64c6d67e85e0b496a9ac8b108e00151d
Natalie2337 <12012337@mail.sustech.edu.cn>
2024-01-01 00:31:43 +0800

    write a barchart API barChartData
    get TagNames and the three TagValues
    so that we can draw a line graph

--- src/main/java/com/example/java2project/controller/DataController.java
+++ src/main/java/com/example/java2project/controller/DataController.java
@@ -7,13 +7,15 @@ import com.example.java2project.pojo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "localhost")
+
 public class DataController {
 
     @Autowired
@@ -33,6 +35,109 @@ public class DataController {
         }
     }
 
+    @GetMapping("/experimental/barChartData")
+    public Result experimentalBarChartData() {
+        List<Tag> rst = dataService.getTagList();
+        List<Function<String, Number>> abstractMethods = List.of(
+                dataService::getAverageAnswerCountOfTag,
+                dataService::getAverageViewCountOfTag,
+                dataService::getTagProportion
+        );
+        List<List<Number>> rst2 = rst.stream()
+                .map(tag -> abstractMethods.stream().map(m -> m.apply(tag.getTag_name())).toList())
+                .toList();
+        List<String> methodNames = List.of("BaseAnswer", "BaseView", "BaseTag");
+        BarChartData data;
+        data = new BarChartData();
+        data.tagNames = rst.stream().map(Tag::getTag_name).toList();
+        var f = flip(rst2);
+        var f2 = f.stream().map(DataController::normalize).toList();
+        f = f2;
+        if (f.size() != methodNames.size()) {
+            throw new RuntimeException();
+        }
+        List<BarChartData.HotEntry> l = new ArrayList<>();
+        for (int i = 0; i < methodNames.size(); i++) {
+            List<Number> l2 = f.get(i);
+            String str = methodNames.get(i);
+            BarChartData.HotEntry h = new BarChartData.HotEntry();
+            h.hotRuleName = str;
+            h.hotValues = l2;
+            l.add(h);
+        }
+        data.tagValues = l;
+        return Result.success(data);
+    }
+
+    public static List<Number> normalize(List<Number> src) {
+        var minn = src.stream().mapToDouble(Number::doubleValue).min().getAsDouble();
+        var maxn = src.stream().mapToDouble(Number::doubleValue).max().getAsDouble();
+        List<Number> l = new ArrayList<>();
+        for (int i = 0; i < src.size(); i++) {
+            double v = src.get(i).doubleValue();
+            v -= minn;
+            double division = maxn - minn;
+            double rst = v / division;
+            if (rst >= 0 && rst <= 1) {
+
+            } else {
+                rst = -1;
+            }
+            l.add((Double ) rst);
+        }
+        return l ;
+    }
+
+    public static <T> List<List<T>> flip(List<List<T>> src) {
+        List<List<T>> rt = new ArrayList<>();
+        int column = src.size();
+        if (column == 0) {
+            return src;
+        }
+        var first = src.get(0);
+        int row = first.size();
+        for (var i : src) {
+            if (i.size() != row) {
+                throw new RuntimeException();
+            }
+        }
+        for (int i = 0; i < row; ++i) {
+            List<T> b;
+            rt.add(b = new ArrayList<>());
+            for (int j = 0; j < column; ++j) {
+                var idx = src.get(j).get(i);
+                b.add(idx);
+            }
+        }
+        return rt;
+    }
+
+    public static void main(String[] args) {
+        int row = 2;
+        int col = 3;
+        List<List<Number>> values = new ArrayList<>();
+        for (int i = 0; i < row; ++i) {
+            List<Number> f;
+            values.add(f = new ArrayList<>());
+            for (int j = 0; j < col; ++j) {
+                f.add(i + j * 2);
+            }
+        }
+        System.out.println(values);
+        var flip2 = flip(values);
+        System.out.println(flip2);
+    }
+
+    static class BarChartData {
+        public List<String> tagNames;
+        static class HotEntry {
+            public String hotRuleName;
+            public List<Number> hotValues;
+        }
+        public List<HotEntry> tagValues;
+    }
+
+    //下面是三个热度指标
 
     /**
      * 计算tag的平均view-count
@@ -74,6 +179,9 @@ public class DataController {
      */
     @GetMapping("/tag/proportion")
     public Result getTagProportion(String tag) {
+        if (true) {
+            return Result.success(tag.length());
+        }
         try {
             Double res = dataService.getTagProportion(tag);
             return Result.success(res);
